8a778b6e74dde506d277a724a636ef417efffd67
maromaroXD <amine-fb-fb@hotmail.fr>
2022-11-28 00:36:17 +0100

    test lib.

---
+++ src/conf.lua
@@ -0,0 +1,24 @@
+do
+  local _with_0 = love
+  _with_0.conf = function(t)
+    t.title = "AGF"
+    t.author = "Tourahi"
+    t.version = "11.3"
+    t.console = false
+    t.window.width = 500
+    t.window.height = 400
+    t.modules.joystick = false
+    t.modules.audio = true
+    t.modules.keyboard = true
+    t.modules.event = true
+    t.modules.image = true
+    t.modules.graphics = true
+    t.modules.timer = true
+    t.modules.mouse = true
+    t.modules.sound = true
+    t.modules.physics = false
+    t.window.vsync = true
+    t.window.resizable = false
+  end
+  return _with_0
+end
